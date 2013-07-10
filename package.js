c950c608d237bda7c774f55c7bad1c7988f55dd8
Ore Landau <Ore4444@gmail.com>
2013-07-10 11:54:34 +0300

    First commit

---
+++ package.js
@@ -0,0 +1,7 @@
+Package.describe({
+  summary: "Fast n-dimensional filtering and grouping of records."
+});
+
+Package.on_use(function (api, where) {
+  api.add_files('crossfilter/crossfilter.js', 'client');
+});
