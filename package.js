ff481394551f5d02a69e9c0e349c6a426f82bf67
Konstantin Burov <aectann@gmail.com>
2014-07-16 19:41:26 +1000

    Use npm dependency to bind crossfilter package.

--- package.js
+++ package.js
@@ -2,11 +2,11 @@ Package.describe({
   summary: "Fast n-dimensional filtering and grouping of records."
 });
 
-Npm.depends({
-    'crossfilter': '1.3.7' // Where x.x.x is the version, e.g. 0.3.2
-});
 
 Package.on_use(function (api, where) {
+  Npm.depends({
+       'crossfilter': '1.3.7' // Where x.x.x is the version, e.g. 0.3.2
+  });
   api.add_files('crossfilter.js', ['client', 'server']);
   api.export('crossfilter');
 });
